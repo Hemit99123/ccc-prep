57ad5ce7c8cc08481c71085f1e29e4df3f7f64c8
Hemit Patel <patelvhemit@gmail.com>
2024-09-12 21:41:59 -0400

    Create tournament-selection.py

---
+++ tournament-selection.py
@@ -0,0 +1,19 @@
+def Solution():
+    wins = 0
+
+    for i in range(6):
+        win_or_loss = input('')
+
+        if win_or_loss == "W":
+            wins += 1
+
+    if wins == 5 or wins == 6:
+        return 1
+    elif wins == 3 or wins == 4:
+        return 2
+    elif wins == 1 or wins == 2:
+        return 3
+    else:
+        return -1
+
+print(Solution());
