5292f0332cac4ea13704f711b378678c2485d3eb
Hemit Patel <patelvhemit@gmail.com>
2024-09-21 19:38:43 -0400

    Create deliv-e-droid.py

---
+++ deliv-e-droid.py
@@ -0,0 +1,31 @@
+# Hemit Patel
+# STUDENT NAME
+# ICS3U0-1 (Course code)
+# J1 Solution
+# TEACHER NAME
+# 21 sep 2024
+
+# The inputs for the program to work
+
+P = int(input(""));
+C = int(input(""));
+F = 0;
+
+# Providing 50 points per package delivered which is expressed in P
+
+F = P * 50
+
+# Now we figure out how much points we must lose because of collisons
+
+points_lost = C * 10
+
+# Subtract the amount of points that need to be lost from our total sum to get accurate score
+
+F = F - points_lost
+
+# Now employing conditonal logic to ensure bonus 500 points are earned based on cretrion
+
+if P > C:
+    F += 500
+
+print(F)
