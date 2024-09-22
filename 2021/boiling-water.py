0f9a2eee14dd59086c41dcaa259405164df8e0cf
Hemit Patel <66741904+Hemit99123@users.noreply.github.com>
2024-09-22 18:27:02 -0400

    2021 folder and j1

---
+++ 2021/boiling-water.py
@@ -0,0 +1,19 @@
+# Hemit Patel
+# STUDENT NAME
+# ICS3U0-1 (Course code)
+# J1 2021 Solution
+# INSTRUCTOR NAME
+# 22 sep 2024
+
+B = int(input(""));
+
+P = 5 * B - 400
+
+print(P)
+
+if P < 100:
+    print(1)
+elif P > 100:
+    print(-1)
+else:
+    print(0)
