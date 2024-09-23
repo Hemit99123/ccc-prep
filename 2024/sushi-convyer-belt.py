0b05167c2fe22c963d89f1d3f5458e2d5f61de8b
Hemit Patel <66741904+Hemit99123@users.noreply.github.com>
2024-09-22 21:01:08 -0400

    added 2024 folder + sushi conveyor built issue

---
+++ 2024/sushi-convyer-belt.py
@@ -0,0 +1,16 @@
+# Hemit Patel
+# STUDENT NUMBER
+# ICS3U0-1 (Course code)
+# J1 2024 Solution
+# INSTRUCTOR NAME
+# 22 sep 2024
+
+R = int(input(""));
+G = int(input(""));
+B = int(input(""));
+
+C = 0;
+
+C = (R * 3) + (G * 4) + (B * 5);
+
+print(C)
