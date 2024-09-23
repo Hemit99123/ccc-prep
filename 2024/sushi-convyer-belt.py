6f6f8f766ad7c9b0831ac6134915ef8d277bc58d
Hemit Patel <patelvhemit@gmail.com>
2024-09-22 21:03:33 -0400

    Update sushi-convyer-belt.py

--- 2024/sushi-convyer-belt.py
+++ 2024/sushi-convyer-belt.py
@@ -5,11 +5,15 @@
 # INSTRUCTOR NAME
 # 22 sep 2024
 
+# The inputs for the amount of different colored plates
 R = int(input(""));
 G = int(input(""));
 B = int(input(""));
 
+# The output 
 C = 0;
+
+# The math involved in figuring out the cost based on the amount of plates and their case per unit
 
 C = (R * 3) + (G * 4) + (B * 5);
 
