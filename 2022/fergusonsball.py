68496c68354faff52ee7de21ae043ecfe89c0891
Hemit Patel <patelvhemit@gmail.com>
2024-09-23 22:55:24 -0400

    Update fergusonsball.py

--- 2022/fergusonsball.py
+++ 2022/fergusonsball.py
@@ -5,31 +5,29 @@
 # INSTRUCTOR NAME
 # 21 sep 2024
 
-N = int(input(""));
-A = 0;
+number_players = int(input(""));
+amount = 0;
 
 # Run the same program each time for each player so that we can see each players rating
 
-for _ in range(N):
+for _ in range(number_players):
 
     # This is the 2 inputs which will collect the player data
-    P = int(input(""));
-    F = int(input(""));
+    points = int(input(""));
+    fouls = int(input(""));
 
     # The processing based on the constraints given to see if a rating is above 40 stars
-    S = 0;
-
-    S = (P * 5) - (F * 3);
+    star_rating = (points * 5) - (fouls * 3);
 
     # If it is, increment one to the amount variable which will serve as an output
-    if S > 40:
-        A += 1;
+    if star_rating > 40:
+        amount += 1;
 '''
 Here we are checking if a team is a "gold" team by seeing if the amount of players
 above 40 ratings is the same as the ACTUAL amount of players on said team
 '''
 
-if A == N:
+if amount == number_players:
     print(str(A) + "+");
 else:
     print(str(A));
