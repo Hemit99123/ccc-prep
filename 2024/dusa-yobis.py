b5eee7853731a1927acfaccfe919c8577b496320
Hemit Patel <patelvhemit@gmail.com>
2024-09-28 19:30:09 -0400

    Create dusa-yobis.py

---
+++ 2024/dusa-yobis.py
@@ -0,0 +1,17 @@
+# Hemit Patel
+# STUDENT NUMBER
+# ICS3U0
+# INSTRUCTOR NAME
+# CCC J2 2024
+
+dusa_size = int(input(""))
+
+while True:
+  yobi_size = int(input(""))
+
+  if dusa_size > yobi_size:
+    dusa_size += yobi_size
+  else:
+    break;
+  
+print(dusa_size)    
