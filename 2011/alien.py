30b4b0b2a16116556e894a0e2558174ec9f3a9bf
Hemit Patel <66741904+Hemit99123@users.noreply.github.com>
2024-09-30 22:27:57 -0400

    added 2011 and alien

---
+++ 2011/alien.py
@@ -0,0 +1,20 @@
+# Hemit Patel
+# STUDENT NUMBER
+# J1 2011 CCC
+# INSTRUCTOR NAME
+# 30 sep 2024
+
+antennae = int(input())
+
+eyes = int(input())
+
+if antennae >= 3 and eyes <= 4:
+    print("TroyMartian")
+
+if antennae <= 6 and eyes >= 2:
+    print("VladSaturnian")
+
+if antennae <= 2 and eyes <= 3:
+    print("GraemeMercurian")
+
+    
