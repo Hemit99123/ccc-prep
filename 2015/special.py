159318baa6f6f407c5696a784978ded207245543
Hemit Patel <66741904+Hemit99123@users.noreply.github.com>
2024-10-14 16:02:54 -0400

    removed bad code

--- 2015/special.py
+++ 2015/special.py
@@ -7,50 +7,11 @@
 month = int(input(""))
 day = int(input(""))
 
-
 # The CCC special day in variables to avoid repeating these numbers
-# The 2 is Feb and 18 represents the day 18 so its feb 18
+# The target_month is Feb and target_day represents the day 18 so its feb 18
 
 target_month = 2 
 target_day = 18
-
-# Checking if the current date info is before, after or on these special days
-
-# < means less than so this is before because the month/day would be LOWER
-
-if (month < target_month and day < target_day):
-    print("Before")
-
-# The opposite of the above if block
-
-elif (month > target_month and day > target_day):
-    print("After")
-
-elif (month < target_month and day > target_day):
-    print("Before")
-
-elif (month > target_month and day < target_day):
-    print("After")
-
-elif (month == target_month and day < target_day):
-    print("Before")
-
-elif (month < target_month and day == target_day):
-    print("Before")
-
-elif (month > target_month and day == target_day):
-    print("After")
-
-elif (month == target_month and day > target_day):
-    print("After")
-
-# Otherwise it must be on the day so therefore output "Special"
-
-else:
-    print("Special")
-
-
-## BETTER SOLUTION -> LEARN IT AND COMMENT ##
 
 month = int(input())
 day = int(input())
@@ -65,23 +26,23 @@ if (1 <= month <= 12) and (1<= day <= 31):
     # First checking the months because if months are above or below feb
     # It means the day doesn't matter as it is a smaller unit of time
 
-    if (month > 2):
+    if (month > target_month):
         print("After")
         
-    elif (month < 2):
+    elif (month < target_month):
         print("Before")
         
     else:
 
-        # In this else statement, we assum that the month == 2 because
+        # In this else statement, we assum that the month == target_month because
         # the other if blocks werent true
 
         # Now day does matter because months are the same
 
-        if (day > 18):
+        if (day > target_day):
             print("After")
         
-        elif (day < 18):
+        elif (day < target_day):
             print("Before")
 
         # If days are not greater or lesser than it means both the month and day
