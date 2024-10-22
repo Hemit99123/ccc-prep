a874c60e6c79aec37f35f8038c6ac2d7e9507a70
Hemit Patel <66741904+Hemit99123@users.noreply.github.com>
2024-10-21 20:34:56 -0400

    dfgsgd

--- 2005/the-cell-sell.py
+++ 2005/the-cell-sell.py
@@ -8,19 +8,28 @@ daytime_mins = int(input())
 night_mins = int(input())
 weekend_mins = int(input())
 
-# Plan A costs
+# The different minute costs for Plan A 
+# All cents are in decimal values because the costs are calculated in dollars and cents is a subunit of it
+
+# Here we are checking if the daytime minutes is under the free allocated time which is 100 minutes 
+# If so, the daytime minutes are free
+# Otherwise, remove 100 minutes from the daytime minutes and compute so you are still accounting for the free 100 minutes you get
 
 if (daytime_mins < 100):
     plan_a_daytime = 0
 else:
     plan_a_daytime = (daytime_mins - 100) * .25
 
+
 plan_a_evening = night_mins * .15
 plan_a_weekend = weekend_mins * .20
 
+# Adding all of the minute costs up for a grand total
 total_a_cost = plan_a_daytime + plan_a_evening + plan_a_weekend
 
 # Plan B costs
+
+# same logic for the free minutes as plan a
 
 if (daytime_mins < 250):
     plan_b_daytime = 0
@@ -32,14 +41,23 @@ plan_b_weekend = weekend_mins * .25
 
 total_b_cost = plan_b_daytime + plan_b_evening + plan_b_weekend
 
+# f string is like .format() function but lets you embed variables using {} instead of in a paramater
+# rounding functions are ran with :.NUMBERf 
+
 print(f"Plan A costs {total_a_cost:.2f}")
 print(f"Plan B costs {total_b_cost:.2f}")
+
+
+# Here we check which plan is the (best) a.k.a cheaper hence why we use < symbol
 
 if (total_a_cost < total_b_cost):
     print("Plan A is cheapest.")
 
 elif (total_b_cost < total_a_cost):
     print("Plan B is cheapest.")
+
+# If none of the plans are cheaper than one another, it means the plan cost is equal so use else 
+# because there is no other scenario that could play out
 
 else:
     print("Plan A and B are the same price.")
