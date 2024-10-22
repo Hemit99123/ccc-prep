3ac2ef91a49806866225e596170ecca1bceec8ce
Hemit Patel <patelvhemit@gmail.com>
2024-10-22 18:26:42 -0400

    Create j1reportcard.py

---
+++ mock/2019/j1reportcard.py
@@ -0,0 +1,18 @@
+# Hemit Patel
+# 781159
+# ICS3U0-4
+# MR VEERA
+# 22 oct 2024
+# report card question
+
+cs452_mark = int(input())
+
+phil145_mark = int(input())
+
+
+if (cs452_mark > phil145_mark):
+    print("CS452")
+
+elif (phil145_mark > cs452_mark):
+
+    print("PHIL145")
