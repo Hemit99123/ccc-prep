8cdb680948ed2ad11b20c0b0146ef6088c400c40
Hemit Patel <66741904+Hemit99123@users.noreply.github.com>
2024-10-22 21:08:33 -0400

    afasf

---
+++ mock/2023/sleep-schedule.py
@@ -0,0 +1,20 @@
+# Hemit Patel
+# 781159
+# MR VEERA
+# ICS3U0-4
+# 22 oct 2024
+# sleep schedule 
+
+bedtime_hour = int(input())
+waketime_hour = int(input())
+
+if (waketime_hour < bedtime_hour):
+    # if waketime is less than bedtime hour, it means we are in the next day
+    # so to reflect we are in the next day, we must add 24 to the waketime hour so that we know a day has gone by
+    
+    difference = (waketime_hour + 24) - bedtime_hour
+
+if (20 <= bedtime_hour <= 23) and (6 <= waketime_hour <= 9) and (8 <= difference <= 10):
+    print("Healthy")
+else:
+    print("Unhealthy")
