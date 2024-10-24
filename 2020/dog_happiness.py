77cac3487d1721256bb449a83cd397a0b965809f
Hemit Patel <patelvhemit@gmail.com>
2024-10-24 06:41:11 -0400

    Update dog_happiness.py

--- 2020/dog_happiness.py
+++ 2020/dog_happiness.py
@@ -14,11 +14,11 @@ large_treat = int(input(""));
 # The output is a formula that is given in the question
 # This formula when computed will provide a score that can be used to determine if dog is happy/sad
 
-happiness_score = 1 * small_treat + 2 * medium_treat + 3 * large_treat
+happiness_score = (1 * small_treat) + (2 * medium_treat) + (3 * large_treat)
 
 # Now using the score we can determine if the dog is happy (above 10 score) or sad
 
-if happiness_score >= 10:
+if (happiness_score >= 10):
     print("happy")
 else:
     print("sad")
