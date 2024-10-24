40582d80485575182d34aa4ef985aa6d0c1c3526
Hemit Patel <patelvhemit@gmail.com>
2024-10-23 20:46:49 -0400

    Create apples-oranges.py

---
+++ sweat/apples-oranges.py
@@ -0,0 +1,18 @@
+# Hemit Patel
+# 781159
+# ICS3U0-4
+# MR VEERA
+# 23 oct 2024
+
+'''
+You are given the number of apples and oranges in a basket. If the number of apples is greater than the number of oranges, print "More apples". If the number of oranges is greater, print "More oranges". If they are equal, print "Equal".
+'''
+apples = int(input())
+oranges = int(input())
+
+if (apples > oranges):
+    print("More apples")
+elif (apples < oranges):
+    print("More oranges")
+else:
+    print("Equal")
