8994710d5ec00c60500de33408c99b896ed74ccb
Hemit Patel <patelvhemit@gmail.com>
2024-10-24 06:45:24 -0400

    Create temperature-ranges.py

---
+++ sweat/temperature-ranges.py
@@ -0,0 +1,13 @@
+# Hemit Patel
+# 781159
+# MR VEERA
+# ICS3U0-4
+# safe temperature
+# 24 oct 2024
+
+temperature = int(input("Give me a temperature:"))
+
+if (-20 <= temperature <= 50):
+    print("Safe")
+else:
+    print("Not Safe")
