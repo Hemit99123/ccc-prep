f30d3c0201057b366846010bd3fc97f896fb0894
Hemit Patel <patelvhemit@gmail.com>
2024-10-24 19:07:31 -0400

    Update special.py

--- 2015/special.py
+++ 2015/special.py
@@ -2,19 +2,17 @@
 # 781159
 # ICS3U0-4
 # MR VEERA
-# Special Day
+# special day
+# 25 oct 2024
 
 month = int(input())
 day = int(input())
 
-# PUT ALL CONDITIONS FOR ONE OUTPUT IN ONE IF STATEMENT (FOR TEST)
-# LINK THEM WITH THE BOOLEAN OPERATORS OF AND AND OR
-
 if (month == 2 and day == 18):
     print("Special")
 
-elif (month == 2 and day < 18) or (month < 2):
-    print("Before")
-
-elif (month == 2 and day > 18) or (month > 2):
+elif (month > 2) or (month == 2 and day > 18):
     print("After")
+
+elif (month < 2) or (month == 2 and day < 18):
+    print("Before")
