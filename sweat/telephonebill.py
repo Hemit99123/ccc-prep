36bc23225e894b61c7340ed2ca686ae0f4d1f3c4
Hemit Patel <patelvhemit@gmail.com>
2024-10-24 21:15:46 -0400

    Create telephonebill.py

---
+++ sweat/telephonebill.py
@@ -0,0 +1,32 @@
+# Hemit Patel
+# 781159
+# ICS3U0-4
+# telephone bill
+# MR VEERA
+# 25 oct 2024
+
+calls = int(input())
+
+if (calls <= 400):
+    rent = 400
+else:
+    rent = 500
+
+
+if (calls <= 150):
+    call_cost = 0
+
+# 150 calls were used from above statement
+
+elif (151 <= calls <= 400):
+    call_cost = (calls-150) * 0.8
+
+elif (401 <= calls <= 1000):
+    call_cost = 250 * 0.8 + (calls-400) * 1
+
+else:
+    call_cost = 250 * 0.8 + 600 + (calls-1000) * 1.20
+
+total_price = rent + call_cost
+
+print("Total bill=", total_price)
