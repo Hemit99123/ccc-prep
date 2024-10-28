632787de7560b994078f946e5baf77830ed618d9
Hemit Patel <patelvhemit@gmail.com>
2024-10-28 18:14:23 -0400

    Update terminoffice.py

--- 2004/terminoffice.py
+++ 2004/terminoffice.py
@@ -6,15 +6,18 @@
 # 28 oct 2024
 
 # Input the start and end years
-x = int(input(""))
-y = int(input(""))
+year_started = int(input(""))
+year_ended = int(input(""))
 
 # Loop through each year from x to y inclusive
-for year in range(x, y + 1):
+# to make it inclusive we must add 1 to the higher limit because python discludes that in its computation 
+# using the python complier 
+
+for year in range(x, year_ended + 1):
     # Check if each role's term lines up with the current year
     # to do this, we subtract the current year by the beggining year.
     # the difference from this should be a multiple of the term served in office 
     # as they should be groupings of that term 
   
-    if (year - x) % 3 == 0 and (year - x) % 4 == 0 and (year - x) % 2 == 0 and (year - x) % 5 == 0:
+    if (year - year_started) % 3 == 0 and (year - year_started) % 4 == 0 and (year - year_started) % 2 == 0 and (year - year_started) % 5 == 0:
         print("All positions change in year", year)
