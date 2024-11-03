a13817e0e8c8b18d9af7992186a5163d03ae3334
Hemit Patel <patelvhemit@gmail.com>
2024-11-03 17:42:31 -0500

    Create Rövarspråket.py

---
+++ 2015/Rövarspråket.py
@@ -0,0 +1,36 @@
+# Hemit Patel
+# 781159
+# ICS3U0-4
+# MR VEERA 
+# 3 nov 2024
+# ccc 15 j3
+
+# for each consonant you have a closest voewl and next consonant in the same index but different list 
+# this adds predictiability to our program as for each consonant, it's idx in that list will be the same for clostestVowel and nextConsonant
+consonant      = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']
+closest_vowel   = ['a', 'a', 'e', 'e', 'e', 'i', 'i', 'i', 'i', 'o', 'o', 'o', 'o', 'o', 'u', 'u', 'u', 'u', 'u', 'u', 'u']
+next_consonant  = ['c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z', 'z']
+
+word = input()
+
+# we will add the new characters one by one to this variable by using the increment operator +=
+# this is outside loop so the value doesn't keep resetting each iteration (repeition of the loop, fancy cs term)
+
+new_word = ""
+
+for char in word:
+    
+    if (char in consonant):
+        # finding the index in the consonant
+        idx = consonant.index(char)
+        
+        # now we use that same idx for the other lists which will find the corresponding information based on the consonant 
+        # as the list was designed as such
+        
+        new_word += f"{char}{closest_vowel[idx]}{next_consonant[idx]}"
+    # if a vowel (non-consonant)
+    else:
+        # the rules of the game don't apply so just add the character itself
+        new_word += char
+
+print(new_word)
