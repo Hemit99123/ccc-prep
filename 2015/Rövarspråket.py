b297b3e54e9a90ff849317b0b9b9eb176553007c
Hemit Patel <patelvhemit@gmail.com>
2024-11-03 17:43:58 -0500

    Update Rövarspråket.py

--- 2015/Rövarspråket.py
+++ 2015/Rövarspråket.py
@@ -19,6 +19,9 @@ word = input()
 new_word = ""
 
 for char in word:
+    # the in operator will check if the character is an item within the list of consonant 
+    # since the consonant is a list of all aviable consonant, this will effectively check for if char is a consonant
+    # we need to do this so we can ensure that only consonants have the rules of the game applied to it (as speificed in problem set)
     
     if (char in consonant):
         # finding the index in the consonant
@@ -30,7 +33,7 @@ for char in word:
         new_word += f"{char}{closest_vowel[idx]}{next_consonant[idx]}"
     # if a vowel (non-consonant)
     else:
-        # the rules of the game don't apply so just add the character itself
+        # the rules of the game don't apply so just add the character itself (no modifications needed!!!)
         new_word += char
 
 print(new_word)
