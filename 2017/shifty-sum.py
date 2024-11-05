4e1c7cfb98a4de4d128c2738a4e5a23f4e117505
Hemit Patel <patelvhemit@gmail.com>
2024-11-04 20:54:43 -0500

    Update shifty-sum.py

--- 2017/shifty-sum.py
+++ 2017/shifty-sum.py
@@ -2,7 +2,7 @@
 # 781159
 # ICS3U0-4
 # MR VEERA
-# shifty sum
+# shifty total
 # 28 oct 2024 
 
 number = int(input())
@@ -11,9 +11,9 @@ amount_shifted = int(input())
 # this is to keep track of the different numbers
 
 # this is our total sum 
-sum = number
+total = number
 
-# this is the previous number that was added to the sum
+# this is the previous number that was added to the total
 prev_num_added = number
 
 # in this loop we will "shift" the number by the amount of times the user inputted
@@ -22,10 +22,10 @@ prev_num_added = number
 for number in range(amount_shifted):
   # we shift the digit by 10 by multiplying 10 here
   current_num = prev_num_added * 10
-  # add to the sum by incrementing it using the +=
-  sum += current_num
-  # set the now previous number added to the sum to the current number
+  # add to the total by incrementing it using the +=
+  total += current_num
+  # set the now previous number added to the total to the current number
   prev_num_added = current_num
 
-# print out the sum
-print(sum)
+# print out the total
+print(total)
