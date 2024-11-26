a12ca2bdbe1dd5bf12d8c1bee13dbcacaa4e4a8c
Hemit Patel <patelvhemit@gmail.com>
2024-11-26 11:06:53 -0500

    Update vote-count.py

--- 2014/vote-count.py
+++ 2014/vote-count.py
@@ -5,10 +5,8 @@
 # VOTE COUNT 
 # 14 oct 2024
 
-# No need for the amount of votes (not using it) -> still got 50/50
-amount_votes = int(input("Enter the amount of votes"))
-
-vote_seq = input("Enter the vote sequence")
+amount_votes = int(input(""))
+vote_seq = input("")
 
 a_count = 0
 b_count = 0
@@ -17,34 +15,28 @@ b_count = 0
 # Go through each character to figure out if it is A or B
 # And increment the respective counter
 
-for i in vote_seq:
-    if (i == "A"):
+for vote in vote_seq:
+    if (vote == "A"):
         a_count += 1
     # Could also use else but what if the character was not B?
     # It would still increment the b count because it is any 
     # scenario other than i == A
 
-    elif (i == "B"):
+    elif (vote == "B"):
         b_count +=1
 
 
-# Now output results...
+# Now see the results
 
+if (a_count == b_count):
+    print("Tie")
 
+elif (a_count > b_count):
+    print("A")
 
 # A count is less than b count meaning b count is higher
 # Did it this way instead of b_count > a_count to be consistent
 # With my elif statements for better readability
 
-if (a_count < b_count):
+elif (a_count < b_count):
     print("B")
-
-# Tie as both counts are ==
-
-elif (a_count == b_count):
-    print("Tie")
-
-# A count is more in this case because > == greater than
-
-elif (a_count > b_count):
-    print("A")
