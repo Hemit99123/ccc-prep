228f7cf1a8bab2025214622308fd0f6057f25d22
Hemit Patel <patelvhemit@gmail.com>
2024-11-27 11:17:45 -0500

    revert

--- 2022/harp-tuning.py
+++ 2022/harp-tuning.py
@@ -3,27 +3,28 @@
 # ICS3UO-1
 # Harp Tuning Convertor
 # INSTRUCTOR NAME
-# 27 nov 2024
+# 26 sep 2024
 
 tuning_instruction = input("")
 
 current_instruction = ""
 
 # Loop through each character in the tuning instruction
-# We use range, so we get a numerical value that represents the current
-# position of the string 
-
 for idx in range(len(tuning_instruction)):
-    character = tuning_instruction[idx]
-    next_character = tuning_instruction[idx+1]
     
-    if character == "+":
+    if tuning_instruction[idx] == "+":
+        
         current_instruction += " tighten "
-    elif character == "-":
+        
+    elif tuning_instruction[idx] == "-":
+        
         current_instruction += " loosen "
+        
     else:
-        current_instruction += character
+        
+        current_instruction += tuning_instruction[idx]
+        
+    if (idx + 1 == len(tuning_instruction) or tuning_instruction[idx+1] in "ABCDEFGHIJKLMNOPQRSTUVWXYZ") and (tuning_instruction[idx] in "0123456789"):
 
-    if (character in "0123456789" and next_character in "ABCDEFGHIJKLMNOPQRSTUVWXYZ") or (idx + 1 == len(tuning_instruction)):
         print(current_instruction)
         current_instruction = ""
