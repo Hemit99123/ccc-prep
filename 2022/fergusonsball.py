d028fa595877e67b0ee1693b562d0832c8f366ec
Hemit Patel <66741904+Hemit99123@users.noreply.github.com>
2024-11-29 20:27:25 -0500

    fix git conflcit

--- 2022/fergusonsball.py
+++ 2022/fergusonsball.py
@@ -18,7 +18,28 @@ for _ in range(players):
         star_players += 1
 
 if (star_players == players):
+    print(str(star_players) + "+")
+# 781159
+# ICS3U0-4
+# MR VEERA
+# 29 oct 2024 
+# fergusonball rating 
+
+players = int(input())
+
+star_players = 0
+
+for _ in range(players):
+    points = int(input())
+    fouls = int(input())
+
+    scores = (points * 5) - (fouls * 3)
+    if (scores > 40):
+        star_players += 1
+
+if (star_players == players):
     print(f"{star_players}+")
 else:
+    print(str(star_players))
     print(star_players)
         
