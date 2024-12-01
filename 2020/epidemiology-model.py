c124705c4f41a877747e6e6795839ef37191829c
Hemit Patel <66741904+Hemit99123@users.noreply.github.com>
2024-12-01 16:41:36 -0500

    made code better

--- 2020/epidemiology-model.py
+++ 2020/epidemiology-model.py
@@ -6,21 +6,15 @@
 # 2 dec 2024
 
 
-# All the inputs required by the program
-people_infected_target = int(input());
+# Input
+target_infections = int(input())  # Target total number of people infected
+initial_infections = int(input())  # People infected on Day 0
+infection_rate = int(input())  # Number of people each infected person infects
 
-people_infected_day0 = int(input());
-
-people_infected_rate = int(input());
-
-# The day counter, it will be incremented everytime the loop is able to run
-days = 0
-
-# This is used to keep reference of the how many were infected in the previous day, starts with the ones infected on day 0
-prev_day_infected = people_infected_day0
-
-# The total amount infected as the days go on
-total_infected = people_infected_day0
+# Initialize variables
+total_infected = initial_infections  # Total number of people infected so far
+current_day_infections = initial_infections  # Infections on the current day
+day = 0  # Start counting from Day 0
 
 # We use a while loop because we have a condition for when the loop ends, meaning we do not know EXACTLY 
 # how many iterations there will be, just when it will break
@@ -31,18 +25,23 @@ total_infected = people_infected_day0
 # The loop will only continue if the total infected has NOT surpassed the people infected target as 
 # if that were the case, we have found the amount of days already (the end condition)
 
-while (total_infected <= people_infected_day0):
+while total_infected <= target_infections:
+
+    # Increment one day as a day has just passed by with each iteration
+
+    day += 1  # Move to the next day
 
     # Calculating how many people were infected on a speific day through a pattern which includes the previous days infected amount * infected rate
-    people_infected_on_day = prev_day_infected * people_infected_rate
 
-    # Update the total infected amount by increment the newly infected people
-    total_infected += people_infected_on_day
+    current_day_infections *= infection_rate  
 
-    # Increment one day as a day has just based
-    days += 1
+    # Update total infections with the current day infection to keep it for record 
+    # We need this number to be updated as it is used as a counter for 
+    # our while loop to ensure the loop stops at the correct time
 
-    # Changing the prev day to this day as this execution of the loop as now stopped
-    prev_day_infected = people_infected_on_day
+    total_infected += current_day_infections 
 
-print(days)
+# Output the first day when the total exceeds the target 
+# (as the while loop will stop executing by then)
+
+print(day)
